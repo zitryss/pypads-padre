880f748a76a8314311f5d0fc65e84c56e94108e5
Eugene Russkikh <Evgenii.Russkikh@msg.group>
2021-02-04 22:57:07 +0100

    Save information to db

--- pypads_padre/injections/loggers/roc_curve.py
+++ pypads_padre/injections/loggers/roc_curve.py
@@ -1,25 +1,17 @@
-import uuid
-from typing import Type, Optional, Union, List
+from typing import Type, Optional
 
 from pydantic import BaseModel
 from pypads.app.env import InjectionLoggerEnv
 from pypads.app.injections.injection import InjectionLogger
 from pypads.app.injections.tracked_object import TrackedObject
 from pypads.model.logger_output import OutputModel, TrackedObjectModel
-from pypads.model.models import IdReference, BaseStorageModel
+from pypads.model.models import IdReference
 from pypads.utils.logging_util import FileFormats
 
 
 class RocTO(TrackedObject):
     class RocTOModel(TrackedObjectModel):
-        class DecisionModel(BaseStorageModel):  # BaseStorageModel what is this class for? MetricTO lacks it.
-            truth: Union[str, int] = None
-            prediction: Union[str, int] = ...
-
-            # class Config:        # Will the data be written to a database without this class?
-            #     orm_mode = True  # Why is it not placed nested in a TOModel?
-
-        decisions: List[DecisionModel] = []  # Shouldn't a TOModel contain a reference to a TrackedObject class?
+        value: str = ...
 
     @classmethod
     def get_model_cls(cls) -> Type[BaseModel]:
@@ -27,13 +19,11 @@ class RocTO(TrackedObject):
 
 
 class RocILF(InjectionLogger):
-    # _needed_cached = SystemStatsTO.__name__
-
     class RocILFOutput(OutputModel):  # Is it supposed to be a nested class?
-        individual_decisions: Union[List[IdReference], IdReference] = None
+        roc: Optional[IdReference] = None
 
-        # class Config:
-        #     orm_mode = True
+        class Config:
+            orm_mode = True
 
     @classmethod
     def output_schema_class(cls) -> Type[OutputModel]:
@@ -41,9 +31,14 @@ class RocILF(InjectionLogger):
 
     def __post__(self, ctx, *args, _pypads_env: InjectionLoggerEnv,
                  _pypads_artifact_fallback: Optional[FileFormats] = None, _logger_call,
-                 _logger_output, _pypads_result, **kwargs): # Why the order of parameters changes from logger to logger?
-        pr = _pypads_result  # holds returned value of the function being tracked
-        lo = _logger_output  # RocILFOutput initialized with the ref to RocTO
+                 _logger_output, _pypads_result,
+                 **kwargs):  # Why the order of parameters changes from logger to logger?
+        # pr = _pypads_result  # holds returned value of the function being tracked
+        # lo = _logger_output  # RocILFOutput initialized with the ref to RocTO
+
+        roc_to = RocTO(parent=_logger_output)
+        roc_to.value = "hello, world!"
+        _logger_output.roc = roc_to.store()
 
         # Many splits
         # decisions = RocTO(split_id=uuid.UUID(split_id), parent=_logger_output)
